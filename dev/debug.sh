ae6ff5d8684d13b0b766c123c562780451f6e23e
Jason Foster <jason@djfoster.net>
2021-06-13 01:13:26 +0200

    create new klinger hybrid strategy. resolves #18

--- dev/debug.sh
+++ dev/debug.sh
@@ -5,12 +5,14 @@ CONFIGS="-c user_data/config.base.json
          -c user_data/config.currencyUSDT.json 
          -c user_data/config.exchangeBinance.json"
 BACKTESTING_TIMERANGE="--timerange 20210510-20210530"
-BACKTESTING_STRATEGY="--strategy StrategyKlinger"
+BACKTESTING_STRATEGY="--strategy StrategyKlinger1db4hs"
 DEBUGPY_SETTINGS="--listen 5678 --wait-for-client"
+
+source .env/bin/activate
 
 case $* in
 --backtest|-b)
-source .env/bin/activate
+
 python -m debugpy ${DEBUGPY_SETTINGS} -m freqtrade backtesting  --export=trades --timeframe 5m ${BACKTESTING_STRATEGY} ${CONFIGS} ${BACKTESTING_TIMERANGE}
 ;;
 --trade|-t)
