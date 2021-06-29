5362ba2330f9d961d3dc46a105da7d08999d9eb2
Jason Foster <jason@djfoster.net>
2021-06-29 23:07:29 +0200

    optimize strategy

--- dev/user_data/strategies/StrategyKlingerStochWBtc2.py
+++ dev/user_data/strategies/StrategyKlingerStochWBtc2.py
@@ -8,13 +8,16 @@ from pandas.core.frame import DataFrame
 from mcDuck.custom_indicators import merge_dataframes, stoch_rsi_smooth, klinger_oscilator
 from functools import reduce
 
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent))
+
+from StrategyKlingerStochWBtc import StrategyKlingerStochWBtc
 
 """
-Buys when the 1D Klinger and the 4H Klinger crosses
+Optimized for use with BTC
 """
-
-
-class StrategyKlingerStochWBtc2(IStrategy):
+class StrategyKlingerStochWBtc2(StrategyKlingerStochWBtc):
     INTERFACE_VERSION = 2
 
     # Optimal ticker interval for the strategy.
@@ -50,83 +53,4 @@ class StrategyKlingerStochWBtc2(IStrategy):
     trailing_stop_positive_offset = 0.322
     trailing_only_offset_is_reached = True
 
-    def informative_pairs(self):
-        pairs = self.dp.current_whitelist()
-        informative_pairs = [(pair, self.timeframe_main) for pair in pairs] + \
-                            [(pair, self.timeframe_support) for pair in pairs]
-        informative_pairs.append(("BTC/USDT", self.timeframe_support))
-        return informative_pairs
-
-    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        if self.config['runmode'].value in ('backtest', 'hyperopt'):
-            assert (timeframe_to_minutes(self.timeframe) <=
-                    5), "Backtest this strategy in 5m or 1m timeframe."
-
-        if not self.dp:
-            return dataframe
-
-        dataframe_main = self.dp.get_pair_dataframe(
-            pair=metadata['pair'],
-            timeframe=self.timeframe_main
-        )
-        [dataframe_main["main_kvo"], dataframe_main["main_ks"]] = \
-            klinger_oscilator(dataframe_main)
-
-        dataframe_main = stoch_rsi_smooth(dataframe_main)
-
-        btc_dataframe = self.dp.get_pair_dataframe(
-            pair="BTC/USDT",
-            timeframe=self.timeframe_support
-        )
-        [btc_dataframe["btc_kvo"], btc_dataframe["btc_ks"]] = klinger_oscilator(btc_dataframe)
-        #btc_dataframe = stoch_rsi_smooth(btc_dataframe)
-        #btc_dataframe.rename(inplace=True,columns={"stochk":"btc_stochk","stochd": "btc_stochd"})
-        dataframe = merge_dataframes(
-            source=btc_dataframe,
-            sourceTimeframe=self.timeframe_support,
-            destination=dataframe,
-            destinationTimeFrame=self.timeframe
-        )
-
-        dataframe = merge_dataframes(
-            source=dataframe_main,
-            sourceTimeframe=self.timeframe_main,
-            destination=dataframe,
-            destinationTimeFrame=self.timeframe
-        )
-
-        return dataframe
-
-    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        minimum_coin_price = 0.0000015
-        conditions = []
-        last_candle_main = dataframe.shift(self.shift_value(self.timeframe_main))
-        last_candle_support = dataframe.shift(self.shift_value(self.timeframe_support))
-        conditions.append(dataframe["volume"] > 0)
-        conditions.append(dataframe["close"] > minimum_coin_price)
-        conditions.append(dataframe["stochk"] > last_candle_main["stochk"])
-        conditions.append((last_candle_main['main_kvo'] < last_candle_main['main_ks']) &
-                          (dataframe['main_kvo'] > dataframe['main_ks']))
-        if('btc_stochk' in dataframe.columns):
-            conditions.append(dataframe["btc_stochk"] > last_candle_main["btc_stochk"])
-        if('btc_kvo' in dataframe.columns):
-            conditions.append((last_candle_main['btc_kvo'] < last_candle_main['btc_ks']) &
-                              (dataframe['btc_kvo'] > dataframe['btc_ks']))
-        if conditions:
-            dataframe.loc[reduce(lambda x, y: x & y, conditions), "buy"] = 1
-
-        return dataframe
-
-    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        conditions = []
-        last_candle_main = dataframe.shift(self.shift_value(self.timeframe_main))
-        conditions.append(dataframe["volume"] > 0)
-        conditions.append(dataframe["stochk"] < last_candle_main["stochk"])
-        conditions.append((last_candle_main['main_kvo'] > last_candle_main['main_ks']) &
-                          (dataframe['main_kvo'] < dataframe['main_ks']))
-        if conditions:
-            dataframe.loc[reduce(lambda x, y: x & y, conditions), "sell"] = 1
-        return dataframe
-
-    def shift_value(self, timeframe: str) -> int:
-        return int(ceil(timeframe_to_minutes(timeframe) / timeframe_to_minutes(self.timeframe)))
+   
