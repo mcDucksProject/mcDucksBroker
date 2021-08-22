44416dd99c16db79bfdf92b9927194c6f848c10b
Jason Foster <jason@djfoster.net>
2021-08-22 18:38:38 +0200

    update strategy

--- dev/user_data/strategies/StrategyManual.py
+++ dev/user_data/strategies/StrategyManual.py
@@ -31,10 +31,13 @@ class StrategyManual(IStrategy):
 
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['buy'] = 0
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['sell'] = 0
         return dataframe
